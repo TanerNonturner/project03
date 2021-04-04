9944bb046fef0a219c8c95e11105d3f26dd7a57e
TanerNonturner <tanerdonmez2017@gmail.com>
2021-04-04 14:18:31 +0300

    fourth message from local master

--- src/FirstPackage/Test01.java
+++ src/FirstPackage/Test01.java
@@ -6,7 +6,7 @@ public class Test01 {
 		System.out.println("first commit from local master");
 		System.out.println("second commit from local master");
 		System.out.println("third commit from local master");
-
+		System.out.println("fourth commit from local master");
 	}
 
 }
