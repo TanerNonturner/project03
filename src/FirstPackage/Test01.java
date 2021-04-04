59583ae705394436cee1ca359c58cb69d3fb65f0
TanerNonturner <tanerdonmez2017@gmail.com>
2021-04-04 14:16:49 +0300

    third message from local master

--- src/FirstPackage/Test01.java
+++ src/FirstPackage/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("first commit from local master");
 		System.out.println("second commit from local master");
+		System.out.println("third commit from local master");
 
 	}
 
