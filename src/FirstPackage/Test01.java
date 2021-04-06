75ddc5448125920b4003f73d62f227dc926fb9ac
TanerNonturner <80407730+TanerNonturner@users.noreply.github.com>
2021-04-06 12:21:49 +0300

    conflict

--- src/FirstPackage/Test01.java
+++ src/FirstPackage/Test01.java
@@ -10,7 +10,7 @@ public class Test01 {
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
 		System.out.println("third commit from remote master branch");
-		
+		System.out.println("Here is the conflict from remote master branch");
 	}
 
 }
